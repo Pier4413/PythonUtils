f64fa3d3cd14fd0e892ce5139ec5c63aa20cc5f9
Pier4413 <pierre.delmas@delmasweb.net>
2023-12-30 18:06:25 +0100

    Update the setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='theophanie_utils',
     packages=['theophanie_utils'],
     description='Some utils (starting the app, ...) for the Theophanie Project',
-    version='1.0.0',  # updated
-    url='',
+    version='1.0.1',  # updated
+    url='https://github.com/Pier4413/PythonUtils',
     author='Panda',
     author_email='panda@delmasweb.net',
     keywords=['theophanie']
