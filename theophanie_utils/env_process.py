25879d53c9eeb51e45ee26a8134618801be058aa
Pier4413 <pierre.delmas@delmasweb.net>
2024-01-16 22:24:26 +0100

    Add an optional default value on the environ

--- theophanie_utils/env_process.py
+++ theophanie_utils/env_process.py
@@ -2,7 +2,7 @@ from os import environ
 
 from logger import Logger
 
-def load_an_environ(value : str) -> str | None:
+def load_an_environ(value : str, default_value : str) -> str:
   """
     This function load an element from the environment with checks
 
@@ -16,7 +16,7 @@ def load_an_environ(value : str) -> str | None:
     if data is not None and data != "":
       Logger.debug(value=f"The imported value of {value} is {data}")
       return data
-    return None
+    return default_value
   except Exception as e:
     Logger.warning(value=f"There was an error while loading the env variable : {value}")
-    
+    return default_value
