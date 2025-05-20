780874a7848b8cfc0bd3fc13dae89c35e316ac20
Pier4413 <pierre.delmas@delmasweb.net>
2025-05-20 18:46:22 +0000

    New paradigm for metadata

---
+++ theophanie_utils/__init__.py
@@ -0,0 +1,19 @@
+from theophanie_models.life import Life as LifeMessage
+
+__authors__ = ["Panda <panda@delmasweb.net>"]
+__short_licence_name__ = "No licence"
+__long_licence_text__ = ""
+__version__ = "1.0.0"
+__name__ = "Theophanie Utils"
+
+def create_life_message() -> LifeMessage:
+
+  """
+    Create the life message in the format of models
+  """
+  authors_part = LifeMessage(text="Authors", value=str(__authors__))
+  licence_part = LifeMessage(text="Licence", value=__short_licence_name__)
+  version_part = LifeMessage(text="Version", value=__version__)
+  life_message = LifeMessage(text=__name__, childs=[version_part, authors_part, licence_part])
+
+  return life_message
